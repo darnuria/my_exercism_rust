772c479ef1f2111810669ff86f22e411f8791c65
Axel Viala <axel.viala@darnuria.eu>
2020-11-12 01:08:39 +0100

    fix grains

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,10 +1,10 @@
 
 // Compilation generated solution with a build script.
-// Provides const GRAINS: [63; u64];
+// Provides const GRAINS: [64; u64];
 include!(concat!(env!("OUT_DIR"), "/grains.rs"));
 
 // Hard coding of s = 54 solution to avoid using u128 numbers.
-pub fn square_(s: u32) -> u64 {
+pub fn square_old(s: u32) -> u64 {
     match s {
         0 => panic!("Square must be between 1 and 64"),
         64 => 9_223_372_036_854_775_808u64,
@@ -14,13 +14,16 @@ pub fn square_(s: u32) -> u64 {
 }
 
 #[inline]
-pub fn square_generated(s: u32) -> u64 {
+// Hardcoded solution with maybe totally a not idiomatic way XD
+// But I did'nt wanted to use lazy-static.
+pub fn square(s: u32) -> u64 {
     match s {
         0 => panic!("Square must be between 1 and 64"),
         n if n > 64 => panic!("Square must be between 1 and 64"),
         n => GRAINS[(n -1) as usize]
        }
 }
+
 pub fn total() -> u64 {
     (1..=64).fold(0, |acc, n| acc + square(n))
 }
