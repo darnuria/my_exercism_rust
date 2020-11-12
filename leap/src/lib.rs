2bcc95468c02f255286eb7d86137e44e5779f29b
Axel Viala <axel.viala@darnuria.eu>
2020-11-12 01:11:08 +0100

    Waw so nice

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -9,22 +9,17 @@ pub fn is_not_leap_year(year: u64) -> bool {
     year % 4 != 0 || year % 400 != 0 && year % 100 == 0
 }
 
-
 pub fn is_leap_year_overcharged(y: u64) -> bool {
-    (((y % 4 == 0) as u64)
-      & 
-        ((y % 100 != 0) as u64) 
-        | ((y % 400 == 0) as u64)) == 0
+    (((y % 4 == 0) as u64) & ((y % 100 != 0) as u64) | ((y % 400 == 0) as u64)) == 0
 }
 
-
 pub fn is_leap_year_if(year: u64) -> bool {
-    if year % 100 == 0 && year % 400 != 0{
-        return false
+    if year % 100 == 0 && year % 400 != 0 {
+        return false;
     }
 
     if year % 4 == 0 {
-        return true
+        return true;
     }
 
     return false;
@@ -32,31 +27,27 @@ pub fn is_leap_year_if(year: u64) -> bool {
 
 pub fn is_leap_year_if_not(year: u64) -> bool {
     if year % 4 != 0 {
-        return false
+        return false;
     }
 
-
     if year % 400 != 0 {
-        return false
+        return false;
     }
 
     if year % 100 == 0 {
-        return false
+        return false;
     }
 
     return true;
 }
 
-
 pub fn is_leap_year_group(year: u64) -> bool {
-    let divisible_by = |n| {
-        year % n == 0
-    };
+    let divisible_by = |n| year % n == 0;
 
-    match (divisible_by(4), divisible_by(100), divisible_by(400)) {
-        (true, true, true) => return true,
-        (true, true, false) => return false,
-        (true, false, _) => return true,
-        (_, _, _) => return false
+    match (4 % n, 100 % n, 400 % n) {
+        (0, 0, 0) => true,
+        (0, 0, 0) => false,
+        (0, _, _) => true,
+        (_, _, _) => false,
     }
 }
