279145f5bb6e258d74f97d11320124fbdbae4824
Axel Viala <axel.viala@darnuria.eu>
2021-10-04 14:04:35 +0200

    revision sum of square

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,19 +1,50 @@
-// Much slower but close to the definition.
-pub fn square_of_sum_iterating(n: u32) -> u32 {
+
+pub fn square_of_sum_v0(n: u32) -> u32 {
+    let mut i = 0;
+    let mut sum = 0;
+    while i <= n {
+        sum += i;
+        i += 1; // Can I haz I++? Nay.
+    }
+    sum * sum
+}
+
+pub fn square_of_sum_v1(n: u32) -> u32 {
+    // applique F x, acc -> x + acc, tel que acc = 0 pour 1 à N inclus
+    (1..=n).fold(0, |acc, x| acc + x).pow(2)
+}
+
+pub fn square_of_sum_v2(n: u32) -> u32 {
     (1..=n).sum::<u32>().pow(2)
 }
 
-// Much faster factorised version.
 pub fn square_of_sum(n: u32) -> u32 {
     (n * (n + 1) / 2).pow(2)
 }
 
-pub fn sum_of_squares_iterating(n: u32) -> u32 {
-    (1..=n).fold(0, |acc, n| n.pow(2) + acc)
+pub fn sum_of_squares_v0(n: u32) -> u32 {
+    let mut range = 1..=n;
+    let mut sum = 0;
+    while let Some(x) = range.next() {
+        sum += x * x;
+    }
+    sum
+}
+
+pub fn sum_of_squares_v1(n: u32) -> u32 {
+    let mut sum = 0;
+    for x in 1..=n {
+        sum += x * x;
+    }
+    sum
+}
+
+pub fn sum_of_squares_v2(n: u32) -> u32 {
+    (1..=n).fold(0, |acc, x| acc + x * x)
 }
 
 // geometric:
-// https://www.youtube.com/watch?v=aXbT37IlyZQ&feature=youtu.be
+// https://www.youtube.com/watch?v=aXbT37IlyZQ
 // algebraic:
 // https://www.youtube.com/watch?v=OpA7oNmHobM
 pub fn sum_of_squares(n: u32) -> u32 {
