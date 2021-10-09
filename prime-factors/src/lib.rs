e3ffb6e159ce71445ddf91828879d0a04c3876df
Axel Viala <axel.viala@darnuria.eu>
2021-10-10 00:16:38 +0200

    Use a wheel this time

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,15 +1,23 @@
+// second iteration largely based on this blog article
+// https://programmingpraxis.com/2009/05/08/wheel-factorization/
+// And solution purposed on stackoverflow:
+// https://stackoverflow.com/questions/12756335/fast-prime-factorization-algorithm/12759741#12759741
 pub fn factors(mut n: u64) -> Vec<u64> {
+    let wheel = [1, 2, 2, 4, 2, 4, 2, 4, 6, 2, 6];
+    let mut factor = 2;
+    let mut i = 0;
     let mut v = Vec::new();
-    let mut div = 2;
-    while n >= (div * div) {
-        println!("n: {} div: {}", n, div);
-        if n % div == 0 {
-            v.push(div);
-            n = n / div;
+    while n >= factor * factor {
+        if n % factor == 0 {
+            v.push(factor);
+            n /= factor;
         } else {
-            div = div + 1;
+            factor += wheel[i];
+            i = if i == 10 { 3 } else { i + 1 };
         }
     }
-    if n != 1 { v.push(n);}
+    if n != 1 {
+        v.push(n);
+    }
     v
 }
