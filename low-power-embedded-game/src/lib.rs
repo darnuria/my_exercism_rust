6f1b5cd4b0fa40d374eee45626f605460b724e00
Axel Viala <axel.viala@darnuria.eu>
2021-10-24 21:51:34 +0200

    low-power-embedded-game

---
+++ low-power-embedded-game/src/lib.rs
@@ -0,0 +1,18 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.enumerate().filter(|(i, _)| i % 2 == 0).map(|(_, x)| x)
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        let Position (x, y) = *self;
+        x.abs() + y.abs()
+    }
+}
