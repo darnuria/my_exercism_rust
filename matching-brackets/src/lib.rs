ffd196bce08783406648c69ec5d86e9794649e6d
Axel Viala <axel.viala@darnuria.eu>
2021-12-21 16:35:31 +0100

    Remove cursed version of a recursive bracked algo

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,4 +1,6 @@
-pub fn brackets_are_balanced_stack(string: &str) -> bool {
+// Not too much original stack version
+// Downside: allocate on heap
+pub fn brackets_are_balanced(string: &str) -> bool {
     let mut delimiters = Vec::new();
     for c in string.chars() {
         match c {
@@ -15,24 +17,6 @@ pub fn brackets_are_balanced_stack(string: &str) -> bool {
     }
     delimiters.len() == 0
 }
-use core::str::Chars;
 
-enum Delim {Curly, Squared, Parens}
-fn inner(c: Option<char>, mut s : Chars) -> Option<char> {
-    match (c, s.next()) {
-        (Some(d) , c @ (Some('}') | Some(']') | Some(')'))) => if Some(c) != Some(c) { return Some('@') } else { inner(None, s) },
-        (_ ,Some('{')) => inner(Some('}'), s) ,
-        (_ ,Some('[')) => inner(Some(']'), s) ,
-        (_ ,Some(')')) => inner(Some(')'), s) ,
-        (Some(c), None) => Some(c),
-        (None, None) => None,
-        (None, Some(_)) => inner(None, s),
-        (Some(_), Some(_)) => inner(None, s)
-    }
-}
-
-pub fn brackets_are_balanced(string: &str) -> bool {
-
-    let rest = inner(None, string.chars());
-    rest.is_none()
-}
+// TODO: stack version
+// Upside : dont allocate; may crash on big input if designed too naively.
