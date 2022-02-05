4e7b98c025c0ab8fcf071ca2efbb63d24104fa7f
Axel Viala <axel.viala@darnuria.eu>
2022-02-05 12:48:13 +0100

    Forth Less insane code.

--- forth/src/lib.rs
+++ forth/src/lib.rs
@@ -42,9 +42,75 @@ impl Forth {
     }
 
 
+    fn dup(&mut self) -> Result {
+        let d = *self.stack.last().ok_or(Error::StackUnderflow)?;
+        self.stack.push(d);
+        Ok(())
+    }
+
+    fn over(&mut self) -> Result {
+        self.stack.last().ok_or(Error::StackUnderflow)?;
+        let penult_index = self
+            .stack
+            .len()
+            .checked_sub(2)
+            .ok_or(Error::StackUnderflow)?;
+        let penult = *self.stack.get(penult_index).ok_or(Error::StackUnderflow)?;
+        self.stack.push(penult);
+        Ok(())
+    }
+
+    fn swap(&mut self) -> Result {
+        let len = self.stack.len();
+        let penult_index = len.checked_sub(2).ok_or(Error::StackUnderflow)?;
+        let last_index = len.checked_sub(1).ok_or(Error::StackUnderflow)?;
+        // Awkard overflow check
+        {
+            self.stack.get(penult_index).ok_or(Error::StackUnderflow)?;
+            self.stack.last().ok_or(Error::StackUnderflow)?;
+        }
+        self.stack.swap(last_index, penult_index);
+        Ok(())
+    }
+    fn pop(&mut self) -> Result {
+        self.stack.pop().ok_or(Error::StackUnderflow)?;
+        Ok(())
+    }
+    
+    fn bin_operator(&mut self, op: &[u8]) -> Result {
+        let a = self.stack.pop().ok_or(Error::StackUnderflow)?;
+        let b = self.stack.pop().ok_or(Error::StackUnderflow)?;
+        let result = match op {
+            b"+" => a + b,
+            b"-" => b - a,
+            b"/" => b.checked_div(a).ok_or(Error::DivisionByZero)?,
+            b"*" => a * b,
+            _ => unreachable!(),
+        };
+        self.stack.push(result);
+        Ok(())
+    }
+
     // pub parse_function(&mut self, input: &str) -> Result {
 
     // }
+
+    fn parse(&mut self, instr: &[u8]) -> Result {
+        match instr {
+            b"+" | b"-" | b"/" | b"*" => self.bin_operator(instr),
+            b"dup" => self.dup(),
+            b"drop" => self.pop(),
+            b"swap" => self.swap(),
+            b"over" => self.over(),
+            word if word.iter().all(|&b| (b as char).is_digit(10)) => {
+                let utf8ed_number = unsafe { std::str::from_utf8_unchecked(word) };
+                self.stack
+                    .push(utf8ed_number.parse().map_err(|_| Error::InvalidWord)?);
+                    Ok(())
+            }
+            _ => return Err(Error::UnknownWord),
+        }
+    }
 
     pub fn eval(&mut self, input: &str) -> Result {
         let mut instructions = input.split_whitespace();       
@@ -78,61 +144,12 @@ impl Forth {
                 self.code.insert(name.to_ascii_lowercase(), instructions);
                 continue;
             }
-            let function = self.code.contains_key(&String::from_utf8_lossy(&instr).into_owned());
 
-            match instr {
-                word if function && word.len() > 0 => {
-                    let code = &self.code.get(instruction).cloned().ok_or(Error::UnknownWord)?;
-                    println!("{}", code);
-                    self.eval(code)?;
-                }
-                b"+" | b"-" | b"/" | b"*" => {
-                    let a = self.stack.pop().ok_or(Error::StackUnderflow)?;
-                    let b = self.stack.pop().ok_or(Error::StackUnderflow)?;
-                    let result = match instr {
-                        b"+" => a + b,
-                        b"-" => b - a,
-                        b"/" => b.checked_div(a).ok_or(Error::DivisionByZero)?,
-                        b"*" => a * b,
-                        _ => unreachable!(),
-                    };
-                    self.stack.push(result);
-                }
-                b"dup" => {
-                    let d = *self.stack.last().ok_or(Error::StackUnderflow)?;
-                    self.stack.push(d);
-                }
-                b"drop" => {
-                    self.stack.pop().ok_or(Error::StackUnderflow)?;
-                }
-                b"swap" => {
-                    let len = self.stack.len();
-                    let penult_index = len.checked_sub(2).ok_or(Error::StackUnderflow)?;
-                    let last_index = len.checked_sub(1).ok_or(Error::StackUnderflow)?;
-                    // Awkard overflow check
-                    {
-                        self.stack.get(penult_index).ok_or(Error::StackUnderflow)?;
-                        self.stack.last().ok_or(Error::StackUnderflow)?;
-                    }
-                    self.stack.swap(last_index, penult_index);
-                }
-                b"over" => {
-                    self.stack.last().ok_or(Error::StackUnderflow)?;
-                    let penult_index = self
-                        .stack
-                        .len()
-                        .checked_sub(2)
-                        .ok_or(Error::StackUnderflow)?;
-                    let penult = *self.stack.get(penult_index).ok_or(Error::StackUnderflow)?;
-                    self.stack.push(penult);
-                }
-                word if word.iter().all(|&b| (b as char).is_digit(10)) => {
-                    let utf8ed_number = unsafe { std::str::from_utf8_unchecked(word) };
-                    self.stack
-                        .push(utf8ed_number.parse().map_err(|_| Error::InvalidWord)?);
-                }
-                _ => return Err(Error::UnknownWord),
+            if let Some(code) = self.code.get(instruction).cloned() {
+                println!("{}", code);
+                self.eval(&code)?;
             }
+            self.parse(instr)?
         }
         Ok(())
     }
