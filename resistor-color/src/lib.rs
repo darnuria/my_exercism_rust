6387e80cc519aae11ecfac78372285f026d8c165
Axel Viala <axel.viala@darnuria.eu>
2022-02-23 15:13:33 +0100

    Resistor-color: Test OK.

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -1,28 +1,70 @@
+use core::panic;
+
 #[derive(Debug, PartialEq)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
-pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+fn value_to_color(value: usize) -> ResistorColor {
+    match value {
+        0 => ResistorColor::Black,
+        1 => ResistorColor::Brown,
+        2 => ResistorColor::Red,
+        3 => ResistorColor::Orange,
+        4 => ResistorColor::Yellow,
+        5 => ResistorColor::Green,
+        6 => ResistorColor::Blue,
+        7 => ResistorColor::Violet,
+        8 => ResistorColor::Grey,
+        9 => ResistorColor::White,
+        _ => panic!("Not supposed to happens."),
+    }
+}
+
+pub fn color_to_value(color: ResistorColor) -> usize {
+    color as usize
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    let color = match value {
+        0..=9 => value_to_color(value),
+        _ => return String::from("value out of range"),
+    };
+    let color = match color {
+        ResistorColor::Black => "Black",
+        ResistorColor::Brown => "Brown",
+        ResistorColor::Red => "Red",
+        ResistorColor::Orange => "Orange",
+        ResistorColor::Yellow => "Yellow",
+        ResistorColor::Green => "Green",
+        ResistorColor::Blue => "Blue",
+        ResistorColor::Violet => "Violet",
+        ResistorColor::Grey => "Grey",
+        ResistorColor::White => "White",
+    };
+    String::from(color)
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    vec![
+        ResistorColor::Black,
+        ResistorColor::Brown,
+        ResistorColor::Red,
+        ResistorColor::Orange,
+        ResistorColor::Yellow,
+        ResistorColor::Green,
+        ResistorColor::Blue,
+        ResistorColor::Violet,
+        ResistorColor::Grey,
+        ResistorColor::White,
+    ]
 }
--- resistor-color/tests/resistor-color.rs
+++ resistor-color/tests/resistor-color.rs
@@ -6,37 +6,31 @@ fn test_black() {
 }
 
 #[test]
-#[ignore]
 fn test_orange() {
     assert_eq!(color_to_value(ResistorColor::Orange), 3);
 }
 
 #[test]
-#[ignore]
 fn test_white() {
     assert_eq!(color_to_value(ResistorColor::White), 9);
 }
 
 #[test]
-#[ignore]
 fn test_2() {
     assert_eq!(value_to_color_string(2), String::from("Red"));
 }
 
 #[test]
-#[ignore]
 fn test_6() {
     assert_eq!(value_to_color_string(6), String::from("Blue"));
 }
 
 #[test]
-#[ignore]
 fn test_8() {
     assert_eq!(value_to_color_string(8), String::from("Grey"));
 }
 
 #[test]
-#[ignore]
 fn test_11_out_of_range() {
     assert_eq!(
         value_to_color_string(11),
@@ -45,7 +39,6 @@ fn test_11_out_of_range() {
 }
 
 #[test]
-#[ignore]
 fn test_all_colors() {
     use ResistorColor::*;
     assert_eq!(
