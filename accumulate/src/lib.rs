3adfdfecb89c43e415ea102f4ead07877a0f9467
Axel Viala <axel.viala@darnuria.eu>
2022-03-01 15:21:06 +0100

    Accumulate: add question to myself

--- accumulate/src/lib.rs
+++ accumulate/src/lib.rs
@@ -1,4 +1,4 @@
-/// What should the type of _function be?
+// Question for future me or anybody: How to avoid allocation?
 pub fn map<T, E>(input: Vec<T>, mut function: impl FnMut(T) -> E) -> Vec<E> {
     let mut new = Vec::with_capacity(input.len());
     for i in input.into_iter() {
