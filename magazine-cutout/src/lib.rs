921d473865d10a2279b16c7673bb7f07be620f02
Axel Viala <axel.viala@darnuria.eu>
2022-03-13 23:43:19 +0100

    run-length-encoding: naive encode

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -1,7 +1,5 @@
 // This stub file contains items that aren't used yet; feel free to remove this module attribute
 // to enable stricter warnings.
-#![allow(unused)]
-
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,5 +1,33 @@
 pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {}.", source);
+    let mut iter = source.as_bytes().iter();
+    let first = if let Some(c) = iter.next() {
+        c
+    } else {
+        return String::new();
+    };
+    let buff = String::with_capacity(source.len() / 2);
+
+    let (count, last, mut buff) =
+        source
+            .as_bytes()
+            .iter()
+            .fold((0, first, buff), |(count, last, mut acc), c| {
+                let count = if last == c {
+                    count + 1
+                } else {
+                    if count > 1 {
+                        acc.push_str(&count.to_string());
+                    }
+                    acc.push(*last as char);
+                    1
+                };
+                (count, c, acc)
+            });
+    if count > 1 {
+        buff.push_str(&count.to_string());
+    }
+    buff.push(*last as char);
+    buff
 }
 
 pub fn decode(source: &str) -> String {
