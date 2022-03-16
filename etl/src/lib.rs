396b8deecb22c3db3c366eba531778a345083734
Axel Viala <axel.viala@darnuria.eu>
2022-03-16 14:15:19 +0100

    elt: cargo fmt

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    h.iter().fold(BTreeMap::new(), | mut tree, (&score, chars)| {
+    h.iter().fold(BTreeMap::new(), |mut tree, (&score, chars)| {
         tree.extend(chars.iter().map(|c| (c.to_ascii_lowercase(), score)));
         tree
     })
